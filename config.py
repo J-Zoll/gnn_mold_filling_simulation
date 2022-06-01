8420c51af133d0bafdbd7dbdc46561fd31c2d2ef
J-Zoll <jonas.zoll@student.kit.edu>
2022-06-01 19:41:33 +0200

    Add directory structure and setup

---
+++ config.py
@@ -0,0 +1,8 @@
+import os.path as osp
+from pathlib import Path
+
+DIR_BASE = osp.dirname(osp.realpath(__file__))
+DIR_DATA = str(Path(DIR_BASE) / "data")
+DIR_DATA_RAW = str(Path(DIR_DATA) / "raw")
+DIR_DATA_PROCESSED = str(Path(DIR_DATA) / "processed")
+
---
+++ setup.py
@@ -0,0 +1,15 @@
+from pathlib import Path
+import config
+
+
+def setup_directories():
+    Path(config.DIR_DATA_RAW).mkdir(parents=True, exist_ok=True)
+    Path(config.DIR_DATA_PROCESSED).mkdir(parents=True, exist_ok=True)
+
+
+def main():
+    setup_directories()
+
+
+if __name__ == "__main__":
+    main()
